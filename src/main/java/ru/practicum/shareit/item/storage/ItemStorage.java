86d05c3ac103aece57f9c3303975e63e7393183a
shishkin <alexconermail@gmail.com>
2025-03-27 16:56:01 +0300

    fix gitlab style

--- src/main/java/ru/practicum/shareit/item/storage/ItemStorage.java
+++ src/main/java/ru/practicum/shareit/item/storage/ItemStorage.java
@@ -6,9 +6,14 @@ import java.util.Collection;
 import java.util.Optional;
 
 public interface ItemStorage {
+
     Collection<Item> findAllByUserId(Long userId);
+
     Collection<Item> search(Long userId, String searchText);
+
     Optional<Item> findById(Long userId, Long itemId);
+
     Item create(Long userId, Item item);
+
     Item update(Long userId, Long itemId, Item item);
 }
--- src/main/java/ru/practicum/shareit/user/storage/UserStorage.java
+++ src/main/java/ru/practicum/shareit/user/storage/UserStorage.java
@@ -6,10 +6,16 @@ import java.util.Collection;
 import java.util.Optional;
 
 public interface UserStorage {
+
     Collection<User> findAll();
+
     Optional<User> findById(long userId);
+
     Optional<User> findByEmail(String email);
+
     User create(User user);
+
     User update(long userId, User user);
+
     void deleteById(long userId);
 }
